3c00e1523d1cdd1217ecf13e85a726a61f857724
Mikhail Bogovalov <mbseq@yandex.ru>
2025-12-05 16:31:57 +0000

    prepare validation files and folders

--- src/videos/dto/video.input-dto.ts
+++ src/videos/dto/video.input-dto.ts
@@ -3,8 +3,5 @@ import { AvailableResolutions } from "../types/video";
 export type videoInputDto = {
   title: string;
   author: string;
-  canBeDownloaded: boolean;
-  minAgeRestriction: null;
-  publicationDate: Date;
   availableResolutions: AvailableResolutions[];
 };
