cc266fa48801c25cff9d75929921a2b37c8dc6de
Mikhail Bogovalov <mbseq@yandex.ru>
2025-12-11 01:43:15 +0000

    2456

--- src/posts/validation/input-dto.validation-middleware.ts
+++ src/posts/validation/input-dto.validation-middleware.ts
@@ -5,7 +5,7 @@ const titleValidation = body("title")
     .trim()
     .exists()
     .withMessage({ message: "Name is required" })
-    .notEmpty().withMessage({ message: "Tile can not be empty" })
+    .notEmpty().withMessage({ message: "Title can not be empty" })
     .isLength({ max: 30 })
     .withMessage({
         message: "Name is too long. Should be less 30 symbols",
@@ -13,6 +13,7 @@ const titleValidation = body("title")
 const shortDescriptionValidation = body("shortDescription")
     .exists().withMessage({ message: "Description is required" })
     .trim()
+    .notEmpty().withMessage({ message: "Description can not be empty" })
     .isLength({ max: 100 })
     .withMessage({
         message: "description should be less than 100 symbols",
