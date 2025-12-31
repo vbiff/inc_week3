6765cacf7a09495b7ac137823711021f2bb205e0
Mikhail Bogovalov <mbseq@yandex.ru>
2025-12-31 16:59:01 +0000

    MONGO_URL

--- src/core/config/config.ts
+++ src/core/config/config.ts
@@ -3,6 +3,6 @@ import { config } from "dotenv";
 config();
 
 export const AppConfig = {
-  MONGO_URL: process.env.MONGODB_URI as string,
+  MONGO_URL: process.env.MONGO_URL as string,
   SECRET: process.env.SECRET as string,
 };
