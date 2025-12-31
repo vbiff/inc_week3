ddb9d6f2eefff60e976c3b704e0d75c732f33b8f
Mikhail Bogovalov <mbseq@yandex.ru>
2025-12-31 16:52:14 +0000

    presentation auth

--- src/features/auth/routers/handlers/login-auth-handler.ts
+++ src/features/auth/routers/handlers/login-auth-handler.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from "express";
 import { HttpStatuses } from "../../../../core/types/http-statuses";
 import { authService } from "../../application/command-services/auth-service";
+import { ResultStatus } from "../../../../core/result/resultCode";
+import { resultCodeToHttpException } from "../../../../core/result/resultCodeToHttpException";
 
 export async function loginAuthHandler(req: Request, res: Response) {
   const authResult = await authService.login(req.body);
 
-  if (!authResult) {
-    res.sendStatus(HttpStatuses.UNAUTHORIZED_401);
+  if (authResult.status !== ResultStatus.Success) {
+    res
+      .status(resultCodeToHttpException(authResult.status))
+      .send(authResult.extensions);
     return;
   }
-  res.sendStatus(HttpStatuses.NO_CONTENT_204);
+  res.status(HttpStatuses.OK_200).send(authResult.data?.accessToken);
 }
