bfe85d1b255172850ddce7db5554555712c93915
Mikhail Bogovalov <mbseq@yandex.ru>
2026-01-01 14:12:50 +0000

    userInfo

--- src/features/comments/routers/handlers/get-comment-by-id-handler.ts
+++ src/features/comments/routers/handlers/get-comment-by-id-handler.ts
@@ -1,9 +1,17 @@
 import { commentsQueryRepository } from "../../repositories/commentsQueryRepository";
 import { HttpStatuses } from "../../../../core/types/http-statuses";
 import { Request, Response } from "express";
+import { userQueryRepositoryMongodb } from "../../../users/repositories/user-query-repository-mongodb";
 
 export async function getCommentByIdHandler(req: Request, res: Response) {
-  const comment = await commentsQueryRepository.getCommentById(req.params.id);
+  //get user info
+  const userInfo = await userQueryRepositoryMongodb.findUserByIdForMe(
+    req.user!.id,
+  );
+  const comment = await commentsQueryRepository.getCommentById(
+    req.params.id,
+    userInfo!,
+  );
   if (!comment) {
     res.sendStatus(HttpStatuses.NOT_FOUND_404);
     return;
