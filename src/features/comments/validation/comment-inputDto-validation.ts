8d78225e7aeea80179c8beafc57297a7c7d67135
Mikhail Bogovalov <mbseq@yandex.ru>
2026-01-04 23:00:58 +0000

    30

--- src/features/comments/validation/comment-inputDto-validation.ts
+++ src/features/comments/validation/comment-inputDto-validation.ts
@@ -4,6 +4,8 @@ const contentValidation = body("content")
   .exists()
   .trim()
   .notEmpty()
+  .isLength({ min: 20, max: 300 })
+  .withMessage("Comment between 30 and 300 characters.")
   .isString()
   .withMessage("The content must be String.");
 
