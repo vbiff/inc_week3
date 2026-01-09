e48edd0170a653a937809cbffcab06ac1a20ffe9
Mikhail Bogovalov <mbseq@yandex.ru>
2026-01-09 02:41:53 +0000

    registration validation

--- src/features/auth/validation/registartion-input-validation.ts
+++ src/features/auth/validation/registartion-input-validation.ts
@@ -6,11 +6,15 @@ const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 const loginValidator = body("login")
   .isString()
   .withMessage("Invalid login")
+  .isLength({ min: 3, max: 10 })
+  .withMessage("Invalid login")
   .matches(loginPattern)
   .withMessage("Invalid login");
 
 const passwordValidator = body("password")
   .isString()
+  .withMessage("Invalid password")
+  .isLength({ min: 6, max: 20 })
   .withMessage("Invalid password");
 
 const emailValidator = body("email")
