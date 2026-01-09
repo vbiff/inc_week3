435af77b568bd85533b5a0e4d96a6a1e6fa3591f
Mikhail Bogovalov <mbseq@yandex.ru>
2026-01-09 02:46:24 +0000

    message{{}}

--- src/features/auth/validation/registartion-input-validation.ts
+++ src/features/auth/validation/registartion-input-validation.ts
@@ -5,23 +5,23 @@ const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
 
 const loginValidator = body("login")
   .isString()
-  .withMessage("Invalid login")
+  .withMessage({ message: "Invalid login" })
   .isLength({ min: 3, max: 10 })
-  .withMessage("Invalid login")
+  .withMessage({ message: "Invalid login" })
   .matches(loginPattern)
-  .withMessage("Invalid login");
+  .withMessage({ message: "Invalid login" });
 
 const passwordValidator = body("password")
   .isString()
-  .withMessage("Invalid password")
+  .withMessage({ message: "Invalid password" })
   .isLength({ min: 6, max: 20 })
-  .withMessage("Invalid password");
+  .withMessage({ message: "Invalid password" });
 
 const emailValidator = body("email")
   .isString()
-  .withMessage("Invalid email")
+  .withMessage({ message: "Invalid email" })
   .matches(emailPattern)
-  .withMessage("Invalid email");
+  .withMessage({ message: "Invalid email" });
 
 export const registrationAuthValidator = [
   passwordValidator,
