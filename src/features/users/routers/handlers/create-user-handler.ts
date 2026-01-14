67aeccb4914f02b4cd40fcd06e5fb7b637500797
Mikhail Bogovalov <mbseq@yandex.ru>
2026-01-14 22:48:48 +0000

    string id

--- src/features/users/routers/handlers/create-user-handler.ts
+++ src/features/users/routers/handlers/create-user-handler.ts
@@ -1,19 +1,18 @@
 import { Request, Response } from "express";
 import { HttpStatuses } from "../../../../core/types/http-statuses";
-import { ObjectId } from "mongodb";
 import { userService } from "../../application/command-services/user-service";
 import { userQueryRepositoryMongodb } from "../../repositories/user-query-repository-mongodb";
 import { UserView } from "../../application/queries/dto/output-dto/user-view";
 
 export async function createUserHandler(req: Request, res: Response) {
-  const newUserId: ObjectId | null = await userService.createUser(req.body);
+  const newUserId: string | null = await userService.createUser(req.body);
 
   if (!newUserId) {
     res.sendStatus(HttpStatuses.NOT_FOUND_404);
   }
 
   const newUser: UserView | null =
-    await userQueryRepositoryMongodb.findUserById(newUserId!.toString());
+    await userQueryRepositoryMongodb.findUserById(newUserId!);
 
   if (!newUser) {
     res.sendStatus(HttpStatuses.NOT_FOUND_404);
