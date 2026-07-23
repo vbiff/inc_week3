7385c95d64af9d29b3f553bbdf1053d3a525d944
Mikhail Bogovalov <mbseq@yandex.ru>
2026-07-23 23:26:11 +0100

    11_5

--- src/testing/routers/testing.router.ts
+++ src/testing/routers/testing.router.ts
@@ -6,6 +6,7 @@ import { CommentModel } from "../../features/comments/domain/comment_entity";
 import { UserModel } from "../../features/users/domain/user_entity";
 import { DeviceModel } from "../../features/security/domain/device_entity";
 import { RateLimitModel } from "../../core/middlewares/rate-limit/rate-limit.model";
+import { CommentLikeModel } from "../../features/comments/domain/comment_like_entity";
 
 export const testingRouter: Router = Router({});
 
@@ -16,5 +17,6 @@ testingRouter.delete("/all-data", async (req: Request, res: Response) => {
   await UserModel.deleteMany();
   await DeviceModel.deleteMany();
   await RateLimitModel.deleteMany();
+  await CommentLikeModel.deleteMany();
   res.sendStatus(HttpStatuses.NO_CONTENT_204);
 });
